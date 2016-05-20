02f47cb0da34d0bc7ef35bd73d1fd45170381a3b
Adam Biagianti <adam.biagianti@gsa.gov>
2016-05-20 15:28:37 -0700

    Follows stylelint naming conventions, adds test

---
+++ src/plugins/declaration-block-max-declarations/index.js
@@ -0,0 +1,54 @@
+var stylelint = require('stylelint');
+var utils = stylelint.utils;
+
+var ruleName = 'plugin/declaration-block-max-declarations';
+var messages = {
+  expected: function(actualPropCount, expectedPropCount) {
+    return 'Expected no more than ' + expectedPropCount + ' declaration(s). Found ' + actualPropCount '.';
+  }
+};
+
+function isNumber(maybeNumber){
+  return typeof maybeNumber === 'number' &&
+    !isNaN(maybeNumber) &&
+    isFinite(maybeNumber);
+}
+
+var pluginDefinition = stylelint.createPlugin(ruleName, function(maxDeclarations, options) {
+  var maxDeclInt = parseInt(maxDeclarations);
+
+  return function(css, result) {
+    var validOptions = utils.validateOptions(result, ruleName, {
+      actual: maxDeclInt,
+      possible: [isNumber]
+    });
+
+    function checkMaxDeclarationsInBlock(statement) {
+      // ignore any statements like imports or extends, we only want property
+      // declarations.
+      var statementDecls = statement.nodes.filter(function(node) {
+        return node.type === 'decl';
+      });
+      var blockDeclarations = statementDecls.length;
+
+      if (blockDeclarations > maxDeclInt) {
+        utils.report({
+          node: statement,
+          message: messages.expected(blockDeclarations, maxDeclInt),
+          result: result,
+          ruleName: ruleName
+        });
+      }
+    }
+
+    if (!validOptions) {
+      return;
+    }
+
+    css.walkRules(checkMaxDeclarationsInBlock);
+  }
+});
+
+module.exports = pluginDefinition;
+module.exports.ruleName = ruleName;
+module.exports.messages = messages;
--- src/plugins/declaration-block-max-declarations/test.js
+++ src/plugins/declaration-block-max-declarations/test.js
@@ -9,10 +9,12 @@ testRule(propertyCount.rule, {
     code: '.foo { color: blue; border: 1px solid white; height: 10px; weight: 10px;}'
   }, {
     code: '.foo { @mixin my-mixin(10); background: black; color: white; display: inline-block; float: left; font-family: sans-serif; font-weight: 12px; height: 10px; line-height: 36px; vertical-align: middle; width: 10px;}'
+  }, {
+   code: '.foo { @mixin my-mixin(10); &.bar { color: pink; } color: white; display: inline-block; float: left; font-family: sans-serif; font-weight: 12px; height: 10px; line-height: 36px; vertical-align: middle; width: 10px;}'  
   }],
 
   reject: [{
     code: '.foo { background: black; color: white; display: inline-block; float: left; font-family: sans-serif; font-weight: 12px; height: 10px; line-height: 36px; vertical-align: middle; width: 10px; z-index: 1;}',
-    message: propertyCount.messages.rejected(11, 10)
+    message: propertyCount.messages.expected(11, 10)
   }]
 });
